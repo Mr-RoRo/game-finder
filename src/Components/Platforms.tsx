710fd0f76614e937899bbe7325d766129f74d8a8
Danial <fsoocity@gmail.com>
2024-10-25 15:23:17 +0330

    feat: create a component to show game platform

---
+++ src/Components/Platforms.tsx
@@ -0,0 +1,27 @@
+import { IconType } from "react-icons";
+import { FaWindows, FaPlaystation, FaXbox } from "react-icons/fa";
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+interface Props {
+  platforms: Platform[];
+}
+const Platforms = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+  };
+  return (
+    <div className="flex space-x-2 text-white">
+      {platforms.map((platform) => {
+        const IconComponent = iconMap[platform.slug];
+        return IconComponent ? <IconComponent key={platform.id} /> : null;
+      })}
+    </div>
+  );
+};
+
+export default Platforms;
