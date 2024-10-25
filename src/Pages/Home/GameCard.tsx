dfbc7f6b73321e306136852a88fc1b1bea1649d6
Danial <fsoocity@gmail.com>
2024-10-25 16:15:59 +0330

    feat(GameCard): use imageOpt for better image load

--- src/Pages/Home/GameCard.tsx
+++ src/Pages/Home/GameCard.tsx
@@ -2,6 +2,7 @@ import Platforms from "../../Components/Platforms";
 import { SiMetacritic } from "react-icons/si";
 import { MdAccessTime } from "react-icons/md";
 import { Game } from "./HomePage";
+import imageOpt from "../../Core/ImageUrl";
 
 interface Props {
   game: Game;
@@ -11,7 +12,7 @@ const GameCard = ({ game }: Props) => {
     <div className="card card-compact bg-base-100 shadow-xl h-80 group">
       <figure>
         <img
-          src={game.background_image}
+          src={imageOpt(game.background_image)}
           alt={game.name}
           className="h-60 w-full object-cover transition-transform transform group-hover:scale-110 group-hover:rotate-3"
         />
